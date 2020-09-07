5ccd83001b671aba398b0784b58da8b8e8e8c69a
CJ Avilla <cjavilla@gmail.com>
2020-09-07 10:09:14 -0700

    Make a memory game :)

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Board from './Board.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        🧠Test your memory 🧠
+      </header>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
