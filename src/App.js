efcfbaa0a03d70c7232afc1f29cd0d932e2f7be6
tarmacjupiter <beheryanthony@gmail.com>
2021-06-23 17:12:58 -0500

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import './App.css';
-import Board from './Board.js';
+import React from "react";
+import "./App.css";
+import Board from "./Board.js";
+import Button from "react-bootstrap/Button";
 
 function App() {
+  function shuffle() {
+    window.location.reload(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        🧠Test your memory 🧠
-      </header>
+      <span className="font-link">
+        <header className="App-header">Memory Game</header>
+      </span>
+      <div>
+        <Button color="primary" onClick={shuffle}>
+          Primary
+        </Button>
+      </div>
       <Board />
     </div>
   );
--- src/Board.js
+++ src/Board.js
@@ -1,87 +1,89 @@
-import React from 'react';
-import Card from './Card.js';
-import './Board.css';
+import React from "react";
+import Card from "./Card.js";
+import "./Board.css";
+
+//ASK USER FOR AMOUNT OF CARDS
+let input = prompt("How many pairs would you like:");
+
+console.log(input);
+
+var numbers = [];
+
+for (var i = 0; i <= input - 1; i++) {
+  numbers[i] = i;
+}
+
+console.log(numbers);
 
 class Board extends React.Component {
   constructor(props) {
-    super(props)
-    const fronts = [
-      '💩',
-      '👹',
-      '👿',
-      '🧠',
-      '🚗',
-      '🚲',
-      '🚂',
-      '🧸',
-      '🤨',
-      '🤟',
-      '♥️',
-      '🦟',
-      '🦨',
-    ]
+    super(props);
+    const fronts = numbers;
     const deck = fronts
       .concat(fronts)
       .sort(() => Math.random() - 0.5)
-      .map(f => {
+      .map((f) => {
         return {
           content: f,
           faceUp: false,
-        }
-      })
+        };
+      });
     this.state = {
       deck: deck,
       firstCard: null,
-    }
+    };
   }
 
   flipCardTo(cardIdx, faceUp) {
     this.setState({
       deck: this.state.deck.map((f, i) => {
-        if(i === cardIdx) {
+        if (i === cardIdx) {
           return {
             content: f.content,
             faceUp: !f.faceUp,
-          }
+          };
         } else {
           return f;
         }
-      })
-    })
+      }),
+    });
   }
 
   flip(cardIdx) {
-    if(this.state.firstCard === null) {
-      this.setState({firstCard: cardIdx});
+    if (this.state.firstCard === null) {
+      this.setState({ firstCard: cardIdx });
     } else {
       const firstCardContent = this.state.deck[this.state.firstCard].content;
       const secondCardContent = this.state.deck[cardIdx].content;
-      if(firstCardContent === secondCardContent) {
-        this.setState({firstCard: null});
+      if (firstCardContent === secondCardContent) {
+        this.setState({ firstCard: null });
       } else {
         setTimeout(() => {
-          this.flipCardTo(this.state.firstCard, false)
-          this.flipCardTo(cardIdx, false)
-          this.setState({firstCard: null});
-        }, 3000)
+          this.flipCardTo(this.state.firstCard, false);
+          this.flipCardTo(cardIdx, false);
+          this.setState({ firstCard: null });
+        }, 1500);
       }
     }
 
-    this.flipCardTo(cardIdx, !this.state.deck[cardIdx].faceUp)
+    this.flipCardTo(cardIdx, !this.state.deck[cardIdx].faceUp);
   }
 
-  render () {
+  render() {
     console.log(this.state.firstCard);
-    return (
-      this.state.deck.map((f, i) => {
-        return (<div className="Board">
+    return this.state.deck.map((f, i) => {
+      return (
+        <div className="Board">
           <Card
-            flip={() => {this.flip(i)}}
+            flip={() => {
+              this.flip(i);
+            }}
             content={f.content}
-            faceUp={f.faceUp} />
-        </div>)
-      })
-    )
+            faceUp={f.faceUp}
+          />
+        </div>
+      );
+    });
   }
 }
 
