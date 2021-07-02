569d03847377d1085280f89026c7d23ccdf05101
tarmacjupiter <beheryanthony@gmail.com>
2021-07-01 20:02:30 -0500

    New theme

--- src/App.js
+++ src/App.js
@@ -9,27 +9,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <span className="font-link">
-        <header className="App-header">Memory Game</header>
-      </span>
-      <Shuffle />
-      <Timer />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "50vh",
-          flexWrap: "wrap",
-          alignItems: "flex-start",
-        }}
-      >
-        {" "}
-        <Board key="board" />
+    <div
+      style={{
+        backgroundColor: "#5c5957",
+      }}
+    >
+      <div className="App">
+        <span className="font-link">
+          <header className="App-header">Memory Game</header>
+        </span>
+        <Shuffle />
+        <Timer />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "50vh",
+            flexWrap: "wrap",
+            alignItems: "flex-start",
+          }}
+        >
+          {" "}
+          <Board key="board" />
+        </div>
+        <Modal />
+        {/* <State /> */}
       </div>
-      <Modal />
-      {/* <State /> */}
     </div>
   );
 }
